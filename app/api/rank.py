0a3416d161ffa7bb16a186d79c64aec91d330934
Mawwlle <dumax@v4scale.com>
2022-12-19 18:18:46 +0300

    fix: rank api

--- app/api/rank.py
+++ app/api/rank.py
@@ -1,5 +1,8 @@
 """API для рангов"""
 
+from typing import Any
+
+from asyncpg import Record
 from fastapi import APIRouter, Security, status
 
 from app import services
@@ -21,3 +24,31 @@ async def create_rank(
         raise anauthorized_exception
 
     await services.rank.create(rank, description)
+
+
+@router.get("/rank", status_code=status.HTTP_200_OK)
+async def get_rank(
+    id: int,
+) -> Any | None:
+    """Получение ранга из базы данных по ID (общедоступно)"""
+
+    return await services.rank.get_by(id)
+
+
+@router.get("/rank", status_code=status.HTTP_200_OK)
+async def get_list_rank() -> list[Record]:
+    """Получение ранга из базы данных по ID (общедоступно)"""
+
+    return await services.rank.get_list()
+
+
+@router.delete("/rank", status_code=status.HTTP_200_OK)
+async def delete_rank(
+    name: str, current_user: User = Security(get_current_user, scopes=["administrator"])
+) -> None:
+    """Получение ранга из базы данных по ID (общедоступно)"""
+
+    if not current_user:
+        raise anauthorized_exception
+
+    return await services.rank.delete(name)
--- app/services/rank.py
+++ app/services/rank.py
@@ -1,4 +1,8 @@
 """CRUD над рангами"""
+from typing import Any
+
+from asyncpg import Record
+
 from app.dependencies.db import database
 
 
@@ -13,3 +17,42 @@ async def create(rank: str, description: str) -> None:
             await connection.execute(
                 'INSERT INTO "rank"(name, description) VALUES ($1, $2)', rank, description
             )
+
+
+async def get_by(id: int) -> Any | None:
+    """Rank creation
+
+    :param rank: rank name
+    """
+
+    # делается на очень скорую руку :)
+
+    async with database.pool.acquire() as connection:
+        async with connection.transaction():
+            result = await connection.fetchval(
+                'SELECT  name, description FROM "rank" WHERE id=$1', id
+            )
+    return result
+
+
+async def get_list() -> list[Record]:
+    """Rank creation
+
+    :param rank: rank name
+    """
+
+    async with database.pool.acquire() as connection:
+        async with connection.transaction():
+            result = await connection.fetch('SELECT  name, description FROM "rank"', id)
+    return result
+
+
+async def delete(name: str) -> None:
+    """Rank creation
+
+    :param rank: rank name
+    """
+
+    async with database.pool.acquire() as connection:
+        async with connection.transaction():
+            await connection.execute('DELETE FROM "rank" WHERE "name"=$1', name)
